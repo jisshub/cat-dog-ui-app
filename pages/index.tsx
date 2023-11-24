390b20cc482ce5b39e27fafb11c2c8b4df5cd923
Jissmon Jose <jissmon476@gmail.com>
2023-11-24 16:41:53 +0530

    Refactor animal selector component to fetch cat facts and dog images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
-import Image from 'next/image'
-import styles from '../styles/page.module.css';
+import { useState } from 'react';
 
-export default function Home() {
+export default function AnimalSelector() {
+  const [animal, setAnimal] = useState('');
+  const [factOrImageUrl, setFactOrImageUrl] = useState(''); // Separate state for data
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    
+    if (animal === 'cat') {
+        const catFactResponse = await fetch('https://catfact.ninja/fact');
+        const catFactData = await catFactResponse.json();
+        setFactOrImageUrl(catFactData.fact);
+    } else if (animal === 'dog') {
+        const dogImageResponse = await fetch('https://dog.ceo/api/breeds/image/random');
+        const dogImageData = await dogImageResponse.json();
+        setFactOrImageUrl(dogImageData.message);
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <h1>Hello, Nextjs App</h1>
-    </main>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Choose an animal:
+          <select value={animal} onChange={(e) => setAnimal(e.target.value)}>
+            <option value="">Select...</option>
+            <option value="cat">Cat</option>
+            <option value="dog">Dog</option>
+          </select>
+        </label>
+        <button type="submit">Show me a fact/image</button>
+      </form>
+      
+      {factOrImageUrl && (
+        animal === 'cat' ? (
+          <p>{factOrImageUrl}</p> // Display cat fact
+        ) : (
+          <img src={factOrImageUrl} alt="Random Dog" /> // Display dog image
+        )
+      )}
+    </div>
+  );
 }
